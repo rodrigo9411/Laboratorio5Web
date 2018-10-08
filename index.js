c8d1c315a6352db38a894e601d0cbdc25d247c65
Rodrigo Ramirez <user.name>
2018-10-07 23:01:06 -0600

    Creacion de servidor, agregar métodos de get, arreglo temporal

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const pokemon = [
+    { id: 389, name: 'Torterra'},
+    { id: 392, name: 'Infernape'},
+    { id: 395, name: 'Empoleon'}
+];
+
+app.get('/', (req,res) => {
+    res.send('Hola Muchachas')
+});
+
+app.get('/api/pokemon', (req, res) => {
+    res.send(pokemon);
+});
+
+app.get('/api/pokemon/:id',(req, res) => {
+    const poke = pokemon.find(c => c.id === parseInt(req.params.id));
+    if (!poke) res.status(404).send('The Pokemon was not found');
+    res.send(poke);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Escuchanda en puerto ${port}...`));
+ 
