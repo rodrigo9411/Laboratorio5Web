a82b0010224ca739910ea462ae3c2ae861b75f43
Rodrigo Ramirez <user.name>
2018-10-08 23:37:28 -0600

    Agregar Joi para validaciones, agregar totalidad de campos y método Put

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
 const pokemon = [
-    { id: 389, name: 'Torterra'},
-    { id: 392, name: 'Infernape'},
-    { id: 395, name: 'Empoleon'}
+    { id: 1, name: 'Torterra', number: '389', typing: 'Grass/Ground', baseStatTotal: '525', imageLink: 'https://www.serebii.net/sunmoon/pokemon/389.png'},
+    { id: 2, name: 'Infernape', number: '392', typing: 'Fire/Fighting', baseStatTotal: '534', imageLink: 'https://www.serebii.net/sunmoon/pokemon/392.png'},
+    { id: 3, name: 'Empoleon', number: '395', typing: 'Water/Steel', baseStatTotal: '530', imageLink: 'https://www.serebii.net/sunmoon/pokemon/395.png'}
 ];
 
 app.get('/', (req,res) => {
@@ -25,9 +26,29 @@ app.get('/api/pokemon/:id',(req, res) => {
 });
 
 app.post('/api/pokemon', (req, res) => {
+    
+    const schema = {
+        name: Joi.string().required(),
+        number: Joi.number().required(),
+        typing: Joi.string().required(),
+        baseStatTotal: Joi.string().required(),
+        imageLink: Joi.string().required()
+    };
+
+    const result = Joi.validate(req.body,schema);
+
+    if(result.error){
+        res.status(400).send(res.error.details[0].message);
+        return;
+    }
+    
     const poke = {
         id: pokemon.length + 1,
-        name: req.body.name
+        name: req.body.name,
+        number: req.body.number,
+        typing: req.body.typing,
+        baseStatTotal: req.body.baseStatTotal,
+        imageLink: req.body.imageLink
     }
     pokemon.push(poke);
     res.send(poke);
